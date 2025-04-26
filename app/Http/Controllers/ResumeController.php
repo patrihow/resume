d6105e76a92847998a474fce8ff752112b5511eb
Patricia <pbravo.art@gmail.com>
2025-04-26 11:21:35 -0400

    Configuration de 4 routes dirigées vers mon ResumeController

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-use app\Http\Controllers\Controller;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ResumeController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ResumeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,8 +13,11 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', [ResumeController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/home', [ResumeController::class, 'index']);
+Route::get('/about', [ResumeController::class, 'about']);
+Route::get('/resume', [ResumeController::class, 'resume']);
+Route::get('/projets', [ResumeController::class, 'projets']);
+Route::get('/contact', [ResumeController::class, 'contact']);
+Route::post('/contact', [ResumeController::class, 'contactForm']);
