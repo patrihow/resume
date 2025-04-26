2fdfa00e8d0935f3e92e878c2f2edf09387b1ab1
Patricia <pbravo.art@gmail.com>
2025-04-26 10:54:24 -0400

    Ajout des 4 méthodes dans ResumeController

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -1,13 +1,33 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use app\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ResumeController extends Controller
 {
     public function index()
-{
+    {
     return view('home'); 
-}
+    }
+
+    public function about()
+    {
+    return view('about'); 
+    }
+
+    public function resume()
+    {
+    return view('resume'); 
+    }
+
+    public function projets()
+    {
+    return view('projets'); 
+    }
+
+    public function contact()
+    {
+    return view('contact'); 
+    }
 }
